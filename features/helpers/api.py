448b4db8e50dc59dbd8228ee0ce68f1158767fc1
dunossauro <mendesxeduardo@gmail.com>
2020-08-18 14:59:56 -0300

    Alterando ordem na chamada do clean_database no testes de integração

--- features/helpers/api.py
+++ features/helpers/api.py
@@ -2,8 +2,8 @@ from httpx import delete, post
 
 
 def clean_test_database(base_url):
-    assert delete(base_url + 'remove-users').status_code == 200
     assert delete(base_url + 'remove-todos').status_code == 200
+    assert delete(base_url + 'remove-users').status_code == 200
 
 
 def create_user(base_url, user, codes=[400, 201]):
