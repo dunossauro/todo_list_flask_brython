2dace5c09cde00e521d38a117e9a17ea9a170053
dunossauro <mendesxeduardo@gmail.com>
2020-08-18 20:26:37 -0300

    Black nas migrações

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,7 @@ import logging
 from logging.config import fileConfig
 
 from alembic import context
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
