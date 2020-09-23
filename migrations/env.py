f9416fcea93a5cc440d673d2388d5a875fedb651
André Sacilotti <andre.sacilotti@estudante.ufscar.br>
2020-09-23 09:53:51 -0300

    Migração correta do bd, após alteração do tamanho do campo STATE

--- migrations/env.py
+++ migrations/env.py
@@ -48,7 +48,10 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        compare_type=True
     )
 
     with context.begin_transaction():
@@ -84,7 +87,8 @@ def run_migrations_online():
             connection=connection,
             target_metadata=target_metadata,
             process_revision_directives=process_revision_directives,
-            **current_app.extensions['migrate'].configure_args
+            **current_app.extensions['migrate'].configure_args,
+            compare_type=True
         )
 
         with context.begin_transaction():
